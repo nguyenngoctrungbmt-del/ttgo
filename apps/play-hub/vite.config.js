6409aa5731f19f1e7aa8852bd3a57504d2ffe3e9
Trung Nguyen <nguyenngoctrungbmt@gmail.com>
2026-09-17 21:12:55 +0700

    update game flappy bird

--- apps/play-hub/vite.config.js
+++ apps/play-hub/vite.config.js
@@ -8,12 +8,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
 
-  // URL thực tế:
-  // http://localhost/MyWebsite/ttgo/play-hub/
-  base: '/MyWebsite/ttgo/play-hub/',
+  // Chạy được ở bất kỳ thư mục nào
+  base: './',
 
   build: {
-    // apps/play-hub -> ../../play-hub
     outDir: path.resolve(__dirname, '../../play-hub'),
     emptyOutDir: true,
   },
